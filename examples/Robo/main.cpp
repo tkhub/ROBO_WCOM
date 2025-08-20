9af5e488a1f202a7d9a4c4ba5a387e518da035f4
T.Ino <takap.colonel@gmail.com>
2025-08-21 00:50:53 +0900

    examplesフォルダによる使用例切り替えに対応

---
+++ examples/Robo/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("ROBO_WCOM ROBO Initialized");
+}
+
+void loop()
+{
+// put your main code here, to run repeatedly:
+}
---
+++ lib/ROBO_WCOM/ROBO_WCOM.h
@@ -0,0 +1,4 @@
+#ifndef __ROBO_WCOM_H__
+#define __ROBO_WCOM_H__
+
+#endif /* __ROBO_WCOM_H__ */
